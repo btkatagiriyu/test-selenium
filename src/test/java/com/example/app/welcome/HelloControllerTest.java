448f2824f918efedff507cad6881cb9c2df9326c
btkatagiriyu <katagiriyu@pm.nttdata.co.jp>
2018-01-10 16:20:11 +0900

    test-selenium

---
+++ src/test/java/com/example/app/welcome/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.app.welcome;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class HelloControllerTest {
+
+    @Test
+    public void test() {
+
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference(FirefoxProfile.ALLOWED_HOSTS_PREFERENCE, "localhost");
+        
+        FirefoxDriver driver = new FirefoxDriver(profile);
+        driver.get("http://localhost:8080");
+
+        assertThat(driver.findElementByTagName("h1").getText(), is("Hello world!"));
+        
+    }
+
+}
